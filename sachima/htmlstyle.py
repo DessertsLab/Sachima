83eb80c5c6a5d7ba5cc04cd559cf9e6a6419cc2c
zhangmk <51670196@qq.com>
2018-12-29 18:01:56 +0800

    send decorator and htmlstyle css path fix

--- sachima/htmlstyle.py
+++ sachima/htmlstyle.py
@@ -5,7 +5,12 @@ from premailer import transform
 def apply_html_style(strhtmltablelist,
                      title='Data Reports',
                      cssfile='tohtmlstyle_example.css'):
-    cs = os.path.join(os.path.dirname(__file__), 'views', cssfile)
+    css_file_path = os.path.join('views', cssfile)
+    if os.path.exists(css_file_path):
+        cs = css_file_path
+    else:
+        cs = os.path.join(os.path.dirname(__file__), 'views', cssfile)
+
     with open(cs, 'r', encoding='utf-8') as f:
         css = f.read()
     title = '<div class="tabletitle">'+title+'</div>'
--- sachima/send.py
+++ sachima/send.py
@@ -3,7 +3,7 @@ import datetime
 import yaml
 
 
-def except_send(func):
+def send(func):
     '''
     send msg to sns app called dingding
     https://open-doc.dingtalk.com/docs/doc.htm?spm=a219a.7629140.0.0.2vzwCr&treeId=257&articleId=105735&docType=1
@@ -16,16 +16,20 @@ def except_send(func):
         c = yaml.load(f)
         ERROR_GRP_TOKEN = c['sns']['dingding']['ERROR_GRP_TOKEN']
         INFO_GRP_TOKEN = c['sns']['dingding']['INFO_GRP_TOKEN']
+        SENDING_STR = c['sns']['dingding']['SENDING_STR']
+        ERRSENT_STR = c['sns']['dingding']['ERRSENT_STR']
 
     def wrapper(a):
         try:
             time_str = str(datetime.datetime.now())
-            t = '正在发送报表' + a['handler'] + '...' + time_str
+            t = SENDING_STR.format(a['handler'], time_str)
             sns.send_dingding(t, t, ERROR_GRP_TOKEN)
             return func(a)
         except Exception as ex:
-            title = '报表' + a['handler'] + '报错，请检查！' + str(
-                datetime.datetime.now())
+            title = ERRSENT_STR.format(
+                a['handler'],
+                str(datetime.datetime.now())
+            )
             data = str(ex)
             sns.send_dingding(title, title + data, ERROR_GRP_TOKEN)
             sns.send_dingding(
