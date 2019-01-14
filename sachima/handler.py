b007b4d5c2b1861593d220a6178b8edf2f51b089
zhangmk <51670196@qq.com>
2019-01-14 16:00:58 +0800

    handler comment

--- sachima/handler.py
+++ sachima/handler.py
@@ -6,12 +6,12 @@ class ReportsHandler(object):
         if isinstance(kwargs["handler"], list):
             self.handlers = kwargs["handler"]
         else:
-            self.handlers = [kwargs["handler"]]
+            self.handlers = [kwargs["handler"]]  # only one in list
 
     def handle(self, model_in):
         data_in = model_in
         for handler_str in self.handlers:
-            print("calling handler " + handler_str)
+            print("Calling handler --> " + handler_str)
             m = importlib.import_module("handler." + handler_str, handler_str)
             res = m.run(data_in)  # each handler should has run function
             if res is not None:
