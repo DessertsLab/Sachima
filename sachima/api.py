a970779191db7f6f7ca0d49983ef6d5d79f27903
zhangmk <51670196@qq.com>
2019-01-17 16:52:54 +0800

    PEP8

--- sachima/api.py
+++ sachima/api.py
@@ -10,20 +10,20 @@ from sachima.publish import Publisher
 from sachima.params import Filter, data_wrapper
 
 
-def api(platform='superset', isRun=False, **kw_api):
+def api(platform="superset", isRun=False, **kw_api):
     def wrapper(func):
         @functools.wraps(func)
         def api_called(*args, **kw):
 
             # 调用对应注册接口
-            if platform == 'superset':
+            if platform == "superset":
                 fpath = inspect.getfile(func)
                 fname = os.path.split(fpath)[1]
                 fmod = os.path.splitext(fname)[0]
                 Publisher.to_superset(
-                    name = kw_api.get('name', fmod),
-                    type_ = kw_api.get('type_', 'RPC'),
-                    param = fmod,
+                    name=kw_api.get("name", fmod),
+                    type_=kw_api.get("type_", "RPC"),
+                    param=fmod,
                 )
             else:
                 pass
