b6a84d0f04ba325abae13c0fcb786c068e059371
zhangmk <51670196@qq.com>
2019-01-21 16:21:28 +0800

    add http_server

---
+++ sachima/sachima_http_server.py
@@ -0,0 +1,22 @@
+from nameko.standalone.rpc import ClusterRpcProxy
+
+from sanic import Sanic
+from sanic.response import json
+from sachima import conf
+
+app = Sanic()
+
+CONFIG = conf.get("BROKER")
+
+
+def sachima_rpc_reports(req):
+    with ClusterRpcProxy(CONFIG) as rpc:
+        print(req)
+        res = rpc.data.get_report(req.json)
+        return res
+
+
+@app.route("/reports", methods=["POST"])
+async def sachima(request):
+    print(request)
+    return json(sachima_rpc_reports(request))
