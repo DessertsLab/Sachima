a8406683b3a7af8e85d92fafd0c8826d5ace1f93
zhangmk <51670196@qq.com>
2019-02-15 20:59:48 +0800

    issue template

--- sachima/sachima_http_server.py
+++ sachima/sachima_http_server.py
@@ -11,12 +11,12 @@ CONFIG = conf.get("BROKER")
 
 def sachima_rpc_reports(req):
     with ClusterRpcProxy(CONFIG) as rpc:
-        print(req)
+        # print(req)
         res = rpc.data.get_report(req.json)
         return res
 
 
 @app.route("/reports", methods=["POST"])
 async def sachima(request):
-    print(request)
+    #     print(request)
     return json(sachima_rpc_reports(request))
