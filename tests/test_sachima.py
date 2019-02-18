0d5af33887522c2121de55960178c87a3a3b7a4f
zhangmk <51670196@qq.com>
2019-02-18 17:39:23 +0800

    unittest

--- tests/test_sachima.py
+++ tests/test_sachima.py
@@ -1,2 +1,11 @@
-def test_test():
-    assert 1 == 1
+import unittest
+from sachima import tools
+
+
+class TestA(unittest.TestCase):
+    def test_tools(self):
+        assert tools.lengthOfLongestSubstring("abcabcbb") == 3
+
+
+if __name__ == "__main__":
+    unittest.main()
