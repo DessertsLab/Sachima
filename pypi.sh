e191a5505d975088b75cb14ee13fabe748d820d7
zhangmk <51670196@qq.com>
2019-02-25 16:54:31 +0800

    update pypi add git tab opts

--- pypi.sh
+++ pypi.sh
@@ -1,3 +1,11 @@
 python3 setup.py sdist bdist_wheel
 
 python3 -m twine upload dist/*
+
+
+
+git tag -a 2019.1a10 -m '2019.1a10'
+git push origin --tags
+
+# remove remote tag
+git push origin --delete tag remove
