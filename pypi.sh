99f8501b2ae765759dee1602968ff64a9cc8c931
zhangmk <51670196@qq.com>
2019-03-08 13:50:02 +0800

    2019.3.1

--- pypi.sh
+++ pypi.sh
@@ -4,9 +4,9 @@ python3 -m twine upload dist/*
 
 
 
-git tag -a 19.3.1 -m '19.3.1'
+git tag -a 2019.3.1 -m '2019.3.1'
 git push origin --tags
 
 # remove remote tag
 
-git push origin --delete tag 19.3.1
+git push origin --delete tag 2019.3.1
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="sachima",
-    version="19.3.1",
+    version="2019.3.1",
     author="nocmk2",
     author_email="jianye.zhang@gmail.com",
     description="A package for Data Analyst",
