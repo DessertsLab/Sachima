f81b71bcccb6f15ece7d44162075653ee4dc6cb4
zhangmk <51670196@qq.com>
2019-03-22 13:56:18 +0800

    2019.3.10

--- pypi.sh
+++ pypi.sh
@@ -1,3 +1,6 @@
+# make sure you installed wheel
+# python3 -m pip install --user --upgrade setuptools wheel
+
 python3 setup.py sdist bdist_wheel
 
 python3 -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="sachima",
-    version="2019.3.9",
+    version="2019.3.10",
     author="nocmk2",
     author_email="jianye.zhang@gmail.com",
     description="A package for Data Analyst",
