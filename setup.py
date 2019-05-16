3becab0adcfddaf463c00aa73fbea5fb284db852
zhangmk <51670196@qq.com>
2019-05-16 15:09:39 +0800

    2019.5.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="sachima",
-    version="2019.5.4",
+    version="2019.5.5",
     author="nocmk2",
     author_email="jianye.zhang@gmail.com",
     description="A package for Data Analyst",
