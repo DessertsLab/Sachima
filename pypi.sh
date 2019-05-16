cf27293a28a606d7d5a8d4022964518eb3f0ea38
zhangmk <51670196@qq.com>
2019-05-16 10:11:14 +0800

    2019.5.2

--- pypi.sh
+++ pypi.sh
@@ -7,9 +7,9 @@ python3 -m twine upload dist/*
 
 
 
-git tag -a 2019.5.1 -m '2019.5.1'
+git tag -a 2019.5.2 -m '2019.5.2'
 git push origin --tags
 
 # remove remote tag
 
-git push origin --delete tag 2019.5.1
+git push origin --delete tag 2019.5.2
