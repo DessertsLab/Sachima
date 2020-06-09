b4083ef1d3687f9ab9817887fa06b441a4015b83
nocmk2 <51670196@qq.com>
2020-06-09 13:52:59 +0800

    model progress

--- sachima/model.py
+++ sachima/model.py
@@ -5,6 +5,32 @@ import importlib
 from sachima.params import set_sql_params
 from sachima.log import logger
 from tqdm import tqdm
+import io
+import time
+import logging
+import sys
+import threading
+import itertools
+
+
+class DummyTqdmFile(object):
+    """ Dummy file-like that will write to tqdm
+    https://github.com/tqdm/tqdm/issues/313
+    """
+
+    file = None
+
+    def __init__(self, file):
+        self.file = file
+
+    def write(self, x):
+        # Avoid print() second call (useless \n)
+        # if len(x.rstrip()) > 0:
+        tqdm.write(x)
+        # self.buf = buf.strip("\r\n\t ")
+
+    def flush(self):
+        return getattr(self.file, "flush", lambda: None)()
 
 
 class Data:
@@ -39,9 +65,30 @@ class Data:
                 sql = prefunc(set_sql_params(str_sql, params), params)
             else:
                 sql = set_sql_params(str_sql, params)
+            logger.info("=" * 12 + dataname + "=" * 12)
+            # tqdm.pandas()
+            start = time.time()
+            done = False
 
+            def animate():
+                for c in itertools.cycle(["|", "/", "-", "\\"]):
+                    if done:
+                        break
+                    sys.stdout.write("\rparsing sql " + c)
+                    sys.stdout.flush()
+                    time.sleep(0.1)
+                sys.stdout.write("\rDone!     ")
+
+            t = threading.Thread(target=animate)
+            t.daemon = True
+            t.start()
             chunks = pd.read_sql(sql, datatype, chunksize=100)
+            done = True
             df = pd.DataFrame()
-            for chunk in tqdm(chunks):
+            logging.basicConfig(level=logging.DEBUG, stream=DummyTqdmFile(sys.stderr))
+            log = logging.getLogger(__name__)
+            for chunk in tqdm(chunks, total=200):
                 df = pd.concat([df, chunk])
+            elapsed_time = time.time() - start
+            log.info("{} elapsed_time: {} seconds".format(dataname, elapsed_time))
             self.data = df
