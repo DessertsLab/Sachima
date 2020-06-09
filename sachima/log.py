b6665db8f9be92284f8084811ec619259677461e
nocmk2 <51670196@qq.com>
2020-06-09 16:36:00 +0800

    sql running time

--- sachima/log.py
+++ sachima/log.py
@@ -2,13 +2,34 @@ import logging
 import logging.config
 import sys
 from sachima import conf
+from tqdm import tqdm
+
+
+class DummyTqdmFile(object):
+    """ Dummy file-like that will write to tqdm
+    https://github.com/tqdm/tqdm/issues/313
+    """
+
+    file = None
+
+    def __init__(self, file):
+        self.file = file
+
+    def write(self, x):
+        # Avoid print() second call (useless \n)
+        # if len(x.rstrip()) > 0:
+        tqdm.write(x)
+        # self.buf = buf.strip("\r\n\t ")
+
+    def flush(self):
+        return getattr(self.file, "flush", lambda: None)()
 
 
 LOGGING_CONFIG_DEFAULTS = dict(
     version=1,
     disable_existing_loggers=False,
     loggers={
-        "sachima.root": {"level": "INFO", "handlers": ["file", "console1"]},
+        "sachima.root": {"level": "INFO", "handlers": ["file", "console"]},
         "sachima.error": {
             "level": "INFO",
             "handlers": ["error_console"],
@@ -25,10 +46,16 @@ LOGGING_CONFIG_DEFAULTS = dict(
             "backupCount": 10,
             # "stream": sys.stdout,
         },
-        "console1": {
+        # "console1": {
+        #     "class": "logging.StreamHandler",
+        #     "formatter": "generic",
+        #     "level": "DEBUG",
+        # },
+        "console": {
             "class": "logging.StreamHandler",
             "formatter": "generic",
             "level": "DEBUG",
+            "stream": DummyTqdmFile(sys.stderr),
         },
         "error_console": {
             "class": "logging.StreamHandler",
--- sachima/model.py
+++ sachima/model.py
@@ -13,26 +13,6 @@ import threading
 import itertools
 
 
-class DummyTqdmFile(object):
-    """ Dummy file-like that will write to tqdm
-    https://github.com/tqdm/tqdm/issues/313
-    """
-
-    file = None
-
-    def __init__(self, file):
-        self.file = file
-
-    def write(self, x):
-        # Avoid print() second call (useless \n)
-        # if len(x.rstrip()) > 0:
-        tqdm.write(x)
-        # self.buf = buf.strip("\r\n\t ")
-
-    def flush(self):
-        return getattr(self.file, "flush", lambda: None)()
-
-
 class Data:
     def __init__(self, dataname, datatype, params, prefunc):
         """
@@ -41,7 +21,8 @@ class Data:
         """
         self.source = dataname
         self.type = datatype
-        logger.info("Sql file name: " + dataname + " Datatype: " + str(datatype))
+        title = dataname + " | " + str(datatype)
+        logger.info("=" * 12 + " " + title + " " + "=" * 12)
         if datatype in ("xls", "xlsx"):
             self.data = pd.read_excel(
                 os.path.join(conf.get("PROJ_DIR"), "data", dataname)
@@ -65,30 +46,39 @@ class Data:
                 sql = prefunc(set_sql_params(str_sql, params), params)
             else:
                 sql = set_sql_params(str_sql, params)
-            logger.info("=" * 12 + dataname + "=" * 12)
-            # tqdm.pandas()
             start = time.time()
+
             done = False
 
-            def animate():
+            def animate(log):
+                elapsed_time = 0
                 for c in itertools.cycle(["|", "/", "-", "\\"]):
                     if done:
                         break
-                    sys.stdout.write("\rparsing sql " + c)
+                    sys.stdout.write(
+                        "\r<{}> parsing sql {} {} ms".format(dataname, c, elapsed_time)
+                    )
                     sys.stdout.flush()
                     time.sleep(0.1)
-                sys.stdout.write("\rDone!     ")
+                    elapsed_time += 100
+                log("<{}> parsing elapsed time: {} ms".format(dataname, elapsed_time))
+                # sys.stdout.write("\r\n Done!     ")
 
-            t = threading.Thread(target=animate)
+            t = threading.Thread(target=animate, args=(logger.info,))
             t.daemon = True
             t.start()
+
             chunks = pd.read_sql(sql, datatype, chunksize=100)
             done = True
+            t.join()
             df = pd.DataFrame()
-            logging.basicConfig(level=logging.DEBUG, stream=DummyTqdmFile(sys.stderr))
-            log = logging.getLogger(__name__)
-            for chunk in tqdm(chunks, total=200):
+            logger.info("<{}> start loading data... ".format(dataname))
+            for chunk in tqdm(chunks, total=2000):
                 df = pd.concat([df, chunk])
-            elapsed_time = time.time() - start
-            log.info("{} elapsed_time: {} seconds".format(dataname, elapsed_time))
+            loading_data_elapsed_time = time.time() - start
+            logger.info(
+                "<{}> loading data elapsed time: {} seconds".format(
+                    dataname, loading_data_elapsed_time
+                )
+            )
             self.data = df
