f3217a1b1bb785c7518717f76e42f209a93cad33
zhangmk <51670196@qq.com>
2020-07-05 10:31:19 +0800

    2020.7.5.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="sachima",
-    version="2020.7.5.2",
+    version="2020.7.5.3",
     author="nocmk2",
     author_email="jianye.zhang@gmail.com",
     description="A package for Data Analyst",
