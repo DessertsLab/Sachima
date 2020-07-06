e76e4824384fbcf63f0fe68562d603fb25ccd5e6
nocmk2 <51670196@qq.com>
2020-07-06 17:26:39 +0800

    cli middleware get

--- sachima/cli.py
+++ sachima/cli.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
-def sachima():
-    print("hello sachima")
+import os
+import click
+
+@click.command()
+@click.option('get', help="get sachima middleware from github. example: sachima get DessertsLab/pivot_table")
+def sachima(middleware):
+    os.system("git clone https://github.com/{} middleware".format(middleware))
--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     ],
     install_requires=[
         "aenum",
+        "click"
         # "pandas",
         # "Flask",
         # "nameko",
