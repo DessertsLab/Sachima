df5f8fd81e08f55e5a00a3c096a45d0b21ca209c
nocmk2 <51670196@qq.com>
2020-08-06 13:50:28 +0800

    clean unuse code and comments modify

--- sachima/geo.py
+++ sachima/geo.py
@@ -1,4 +1,3 @@
-# -*- coding: utf8 -*-
 import os
 import requests
 import json
@@ -7,7 +6,6 @@ import time
 from sachima.log import logger
 from sachima import conf
 
-from PIL import Image
 from io import BytesIO
 
 BAIDU_GEO_TOKEN = conf.get("BAIDU_GEO_TOKEN")
@@ -52,10 +50,9 @@ def poi_compound_dict(
     return res
 
 
-# poi
 def poi(lat, lng, keywords, radius=1000):
     """
-    高德地图获取poi信息 \n
+    Get poi information from amap \n
     https://lbs.amap.com/api/webservice/guide/api/search \n
     """
     logger.info((lat, lng, keywords, radius))
@@ -83,7 +80,9 @@ def poi(lat, lng, keywords, radius=1000):
 
 def panorama(lat, lng):
     """
-    全景信息查询
+    Panorama information query
+    return Image Buffer
+    you can open the buffer use PIL.Image.open(buffer)
     """
     if BAIDU_GEO_TOKEN is None:
         logger.info("error: Must config BAIDU_GEO_TOKEN in sachima_config.py")
@@ -92,19 +91,18 @@ def panorama(lat, lng):
     url = "http://api.map.baidu.com/panorama/v2"
     values = {"ak": BAIDU_GEO_TOKEN, "fov": 180, "location": "{},{}".format(lng, lat)}
     try:
-        logger.info("正在获取{},{}全景图片。。。".format(lat, lng))
+        logger.info("Fetching {},{} panorama picture ...".format(lat, lng))
         r = requests.get(url, values).content
         b = BytesIO()
         b.write(r)
-        return Image.open(b)
-        # logger.info(r)
+        return b
     except Exception as e:
         raise e
 
 
 def fetchBaiduLatLng(address):
     """
-    利用baidu map api从网上获取city的经纬度。\n
+    Use baidu map api to get the latitude and longitude of city from the Internet. \n
     http://lbsyun.baidu.com/index.php?title=webapi \n
     """
     if BAIDU_GEO_TOKEN is None:
@@ -135,13 +133,13 @@ def fetchBaiduLatLng(address):
                 "baidu_status": r.get("status"),
             }
     except Exception as e:
-        logger.info("fetchBaiduGeo for %s error: %s 发生异常！" % (address, e))
+        logger.info("fetchBaiduGeo for %s error: %s" % (address, e))
         raise e
 
 
 def fetchQQLatLng(address):
     """
-    利用qq map api从网上获取city的经纬度。
+    Use qq map api to get latitude and longitude from the Internet.
     https://lbs.qq.com/miniProgram/jsSdk/jsSdkGuide/methodGeocoder
     """
     if QQ_GEO_TOKEN is None:
@@ -180,13 +178,14 @@ def fetchQQLatLng(address):
                 "qq_message": r.get("message"),
             }
     except Exception as e:
-        logger.info("fetchQQGeo for %s error: %s 发生异常！" % (address, e))
+        logger.info("fetchQQGeo for %s error: %s！" % (address, e))
         raise e
 
 
 def fetchAmapLatLng(address):
     """
-    使用高德API
+    Use amap api to get latitude and longitude from the Internet.
+    https://lbs.qq.com/miniProgram/jsSdk/jsSdkGuide/methodGeocoder
     """
     logger.info("fetching {} amap geo info".format(address))
     if AMAP_GEO_TOKEN is None:
@@ -195,7 +194,6 @@ def fetchAmapLatLng(address):
     par = {"address": address, "key": AMAP_GEO_TOKEN}
     base = "http://restapi.amap.com/v3/geocode/geo"
     r = requests.get(base, par).json()
-    # logger.info(r)
     geocodes = {}
     GPS = [None, None]
     if r.get("count") != "0":
@@ -218,71 +216,3 @@ def fetchAmapLatLng(address):
         "amap_number": str(geocodes.get("number")),
         "amap_level": geocodes.get("level"),
     }
-
-
-def district(self):
-    self.url = "https://apis.map.qq.com/ws/district/v1/list"
-    try:
-        r = requests.get(self.url, params=self.values).json()
-        return r
-    except:
-        print("获取行政区划发生异常！")
-
-
-class ISO3166(object):
-    def __init__(self):
-        self.china = {
-            "安徽省": "CN-34",  # Anhui
-            "北京市": "CN-11",  # Beijing
-            "重庆市": "CN-50",  # Chongqing
-            "福建省": "CN-35",  # Fujian
-            "甘肃省": "CN-62",  # Gansu
-            "广东省": "CN-44",  # Guangdong
-            "广西壮族自治区": "CN-45",  # Guangxi
-            "贵州省": "CN-52",  # Guizhou
-            "海南省": "CN-46",  # Hainan
-            "河北省": "CN-13",  # Hebei
-            "黑龙江省": "CN-23",  # Heilongjiang
-            "河南省": "CN-41",  # Henan
-            "湖北省": "CN-42",  # Hubei
-            "湖南省": "CN-43",  # Hunan
-            "江苏省": "CN-32",  # Jiangsu
-            "江西省": "CN-36",  # Jiangxi
-            "吉林省": "CN-22",  # Jilin
-            "辽宁省": "CN-21",  # Liaoning
-            "内蒙古自治区": "CN-15",  # Nei Mongol
-            "宁夏回族自治区": "CN-64",  # Ningxia Hui
-            "青海省": "CN-63",  # Qinghai
-            "陕西省": "CN-61",  # Shaanxi
-            "山东省": "CN-37",  # Shandong
-            "上海市": "CN-31",  # Shanghai
-            "山西省": "CN-14",  # Shanxi
-            "四川省": "CN-51",  # Sichuan
-            "天津市": "CN-12",  # Tianjin
-            "新疆维吾尔自治区": "CN-65",  # Xinjiang Uygur
-            "西藏自治区": "CN-54",  # Xizang
-            "云南省": "CN-53",  # Yunnan
-            "浙江省": "CN-33",  # Zhejiang
-            "台湾省": "CN-71",  # Taiwan
-            "香港特别行政区": "CN-91",  # Hong Kong
-            "澳门特别行政区": "CN-92",  # Macao
-        }
-
-
-# class qqgeoAsync(object):
-#     loop = asyncio.get_event_loop()
-#     async def async_get_geo(self,name):
-#         url = 'https://apis.map.qq.com/ws/geocoder/v1/'
-#         params = {
-#             'address': name,
-#             'key': '',
-#             'output': 'json',
-#         }
-#         async with aiohttp.ClientSession(loop=loop) as session:
-#             async with session.get(url, params=params) as response:
-#                 response = await response.json()
-#                 if response['status']!=347 and response['status']!=120:
-#                     print(response)
-#                     return response['result']['location']['lat'], response['result']['location']['lng']
-#                 else:
-#                     return 0,0
