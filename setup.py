7a81bac2f142993243395290b2f9fb0f6c1b910b
nocmk2 <51670196@qq.com>
2020-08-27 11:30:43 +0800

    2020.8.27.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="sachima",
-    version="2020.8.27.2",
+    version="2020.8.27.3",
     author="nocmk2",
     author_email="jianye.zhang@gmail.com",
     description="Better data analysis",
